449d7ca3496106b22036858c360e3d752e295509
Piotr <piotr.maslankiewicz@smartology.net>
2015-10-21 16:08:52 +0100

    Added the whole project from Intellij with subdirectories and first program - HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+    public static void main(String[] args){
+        String helloWorld = "ssij";
+
+        StringPrinter stringPrinter = new StringPrinter(helloWorld);
+        stringPrinter.printString();
+    }
+}
---
+++ src/StringPrinter.java
@@ -0,0 +1,12 @@
+
+public class StringPrinter {
+    private String string;
+
+    public StringPrinter(String string) {
+        this.string = string;
+    }
+
+    public void printString(){
+        System.out.println(this.string);
+    }
+}
